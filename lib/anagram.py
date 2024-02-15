f3a1bac5f42775f821f90aeb12bfe4eb71b8dd35
Joan <joanmuhingo@gmail.com>
2024-02-15 14:02:54 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        word_sorted = sorted(self.word)
+        anagrams = []
+
+        for word in word_list:
+            if word.lower() != self.word and sorted(word.lower()) == word_sorted:
+                anagrams.append(word)
+
+        return anagrams
